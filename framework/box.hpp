11849ca6f84046de7dfb600f8704e67bfe69071d
Zzrop <Silbendrechsler@gmx.de>
2016-06-13 15:30:24 +0200

    Soweit die hpp-gerippe erstellt mit den membervariablen. Als nächsten kommen die konstruktoren...

--- framework/box.hpp
+++ framework/box.hpp
@@ -1,12 +1,18 @@
 #ifndef BOX_BUW_HPP
 #define BOX_BUW_HPP
+#include <glm/vec3.hpp>
 
 
 class Box : public Shape{
 
 public:
-	
+
 	float area() const override;
 	float volume() const override;
+
+protected:
+	glm::vec3 a_;
+	glm::vec3 b_;
+
 }
 #endif
--- framework/sphere.hpp
+++ framework/sphere.hpp
@@ -1,4 +1,17 @@
 #ifndef SPHERE_BUW_HPP
 #define SPHERE_BUW_HPP
+#include <glm/vec3.hpp>
+
+
+class Sphere : public Shape{
+
+public:
+	float area() const override;
+	float volume() const override;
+
+protected:
+	glm::vec3 m_;
+	float r_;
+}
 
 #endif
